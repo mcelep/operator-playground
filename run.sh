3fc124ee16bf2778d714c40fa70cac4352d258fd
Murat Celep <me@mcelep.ch>
2018-11-11 17:54:49 +0100

    move hello opreator files into subfolder

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kubectl create namespace hello
-kubectl -n hello apply -f crd.yaml
-kubectl -n hello apply -f controller.yaml
-kubectl -n hello create configmap hello-controller --from-file=sync.py
-kubectl -n hello apply -f webhook.yaml
-kubectl -n hello apply -f hello.yaml
-
